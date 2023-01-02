d345e50a9390bcbd11904cc123012f828051a275
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-02 15:14:03 -0800

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = f"{word[:3]}"
-word_last_2 = f"{word[7:]}"
-middle_word = f"{word[1:8]}"
+word_last_2 = f"{word[-2:]}"
+middle_word = f"{word[1:-1]}"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
