b095f33a9e665c5d2559e64ada21e2e41c17cd67
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-05 17:36:33 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0;
+    result = 0
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             result += int(sys.argv[i])
