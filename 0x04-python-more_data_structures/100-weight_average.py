91d94b5471eb7df51c6b219dd1737c9b5061846a
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-11 10:10:19 -0800

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+
+    top = 0
+    bottom = 0
+
+    for i in my_list:
+        score = my_list[0]
+        weight = my_list[1]
+
+        top += score * weight
+        bottom += weight
+
+    return (top / bottom)
