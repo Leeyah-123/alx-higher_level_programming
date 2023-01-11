f518ce69e311e87b4ec33f19e4db3a77210bfef2
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-11 10:14:04 -0800

    Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
-        return (0)
+        return 0
 
-    top = 0
-    bottom = 0
+    num = 0
+    den = 0
 
-    for i in my_list:
-        score = my_list[0]
-        weight = my_list[1]
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
 
-        top += score * weight
-        bottom += weight
-
-    return (top / bottom)
+    return (num / den)
