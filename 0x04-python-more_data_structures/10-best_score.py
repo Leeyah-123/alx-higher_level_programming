b2cb537c13a622a33a3dad46fc67fcf63842245e
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-11 09:40:47 -0800

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    keys = list(a_dictionary)
-
-    if len(keys) == 0:
+    if not a_dictionary:
         return (None)
 
-    highest_score = a_dictionary[0]
-    for i in keys:
-        if a_dictionary[i] > highest_score:
-            highest_score = a_dictionary[i]
-
-    return (highest_score)
+    return (max(a_dictionary, key=a_dictionary.get))
