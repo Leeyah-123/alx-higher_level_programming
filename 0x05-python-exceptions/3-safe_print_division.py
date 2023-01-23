5043b0f6a1aad4cd62b31abbdc52f5b84464093c
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-23 07:37:07 -0800

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
