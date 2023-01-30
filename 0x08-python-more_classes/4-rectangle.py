8b62f647e9fcbdaad172ab6f510c7d8d23b87ab5
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-01-30 14:27:38 -0800

    String representation

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -109,10 +109,7 @@ class Rectangle:
 
         rectangle = ""
 
-        if self.width == 0 or self.height == 0:
-            return rectangle
-
         for i in range(self.height):
-            rectangle += ("#" * self.width) + "\n"
+                rectangle += ("#" * self.width) + "\n"
 
         return rectangle[:-1]
